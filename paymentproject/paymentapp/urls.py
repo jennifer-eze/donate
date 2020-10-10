5b0fe37273034c6f90cfcecfa1115b1b8625cf53
Jennifer Eze <ogochukwuj.eze@gmail.com>
2020-10-10 15:32:05 +0100

    feat: html templates

--- paymentproject/paymentapp/urls.py
+++ paymentproject/paymentapp/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .import views
+from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('home/', views.home, name='home'),
-    path('startFund', views.startFund, name='startFund'),
+    path('startFund/', views.startFund, name='startFund'),
 ]
--- paymentproject/paymentapp/views.py
+++ paymentproject/paymentapp/views.py
@@ -3,10 +3,10 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return render(request, '')
+    return render(request, 'paymentapp/index.html')
 
 def home(request):
-    return render(request, '')
+    return render(request, 'paymentapp/home.html')
 
 def startFund(request):
-    return render(request, '')
+    return render(request, 'paymentapp/startFund.html')
