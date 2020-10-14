203888080b81795f74aa312506f2c1801fab94ca
Jennifer Eze <ogochukwuj.eze@gmail.com>
2020-10-14 23:39:26 +0100

    feat: model created

--- paymentproject/paymentapp/models.py
+++ paymentproject/paymentapp/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from PIL import Image
 
 # Create your models here.
+class StartFund(models.Model):
+    name = models.CharField(max_length=150, null=True)
+    fundReason = models.CharField(max_length=500, null=True)
+    image = models.ImageField(default='avatar.jpg', upload_to='profile_pics')
+
+    def __str__(self):
+        return self.name
+
+    def save(self):
+        super().save()
+
+        img = Image.open(self.image.path)
+
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
